15f7dce8bddf6159d7e3d1afbb7e1122fd921200
alexkanastasov <alexkanastasov@gmail.com>
2024-10-23 14:14:11 +0100

    Player turn added

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Let's play Blackjack!");
-        Scanner choice = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         Deck d = new Deck();
         d.shuffle();
 
@@ -18,5 +18,25 @@ public class Main {
         dealerHand.add(d.deal());
         dealerHand.show();
         dealerHand.add(d.deal());
+
+        boolean players_turn = true;
+
+        //Hit or stand
+        while (players_turn){
+            System.out.println("Hit or stand");
+            String choice = input.nextLine();
+
+            if (choice.equals("Hit")){
+                playerHand.add(d.deal());
+                playerHand.show();
+            }
+
+            else{
+                playerHand.show();
+                players_turn = false;
+            }
+        }
+
+
     }
 }
